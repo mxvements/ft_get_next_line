ddd33505495a908c481ee5c02b4bb46d22eebdc4
mxvements <lmmielgo@DESKTOP-57O0OKS>
2023-11-06 17:37:14 +0100

    saved a line

--- get_next_line.c
+++ get_next_line.c
@@ -3,44 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luciama2 <luciama2@student.42madrid>       +#+  +:+       +#+        */
+/*   By: lmmielgo <lmmielgo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 17:00:55 by luciama2          #+#    #+#             */
-/*   Updated: 2023/11/05 19:52:15 by luciama2         ###   ########.fr       */
+/*   Updated: 2023/11/06 17:23:48 by lmmielgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
 
-char	*update_stash(char **stash, char **buff)
-{
-
-}
-
 char	*read_file(int fd, char *stash)
 {
-	char	*buff;
 	size_t	readbytes;
 
-	buff = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	stash = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!stash)
 		return (NULL);
-	readbytes = read(fd, buff, BUFFER_SIZE);
+	readbytes = read(fd, stash, BUFFER_SIZE);
 	if (readbytes > BUFFER_SIZE)//read error
 	{
-		free(buff);
+		free(stash);
 		return (NULL);
 	}
 	if (readbytes == 0) //file ending
 	{
 		return (stash);//placeholder
 	}
-	buff[readbytes] = '\0';
-	stash = update_stash(&stash, &buff);
+	stash[readbytes] = '\0';
+	printf("BYTES_READ:\n%zu\n", readbytes);
+	printf("BUFFER_READ:\n%s\n", stash);
 	return (stash);
 }
-/*
+char	*save_first_line(char *stash)
+{
+	char	*line;
+	size_t	i;
+
+	i = 0;
+	while (stash[i] != '\0')
+	{
+		if (stash[i] == '\n')
+			break;
+		i++;
+	}
+	line = (char *)malloc(sizeof(char) * (i + 1));
+	if (!line)
+		return (NULL);
+	line[i] = '\0';
+	while (--i > 0)
+		line[i] = stash[i];
+	line[0] = stash[0];
+	return (line);	
+}
+
 char	*update_data(char **stash)
 {
 	char	*line;
@@ -49,16 +65,30 @@ char	*update_data(char **stash)
 	if (!line)
 	{
 		free(*stash);
-		free(stash);
-		*stash = NULL;
+		stash = NULL;
 		return (NULL);
 	}
-	*stash = delete_saved_line(*stash);//TODO
+	printf("LINE SAVED:\n%s\n", line);
+	printf("STASH WHEN LINE SAVED:\n%s\n", *stash);
+	/**stash = delete_saved_line(*stash);//TODO
 	if (!(*stash))
-		return (NULL);
+		return (NULL);*/
 	return (line);
 }
-*/
+
+int	ft_strchr(char *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i++] == c)
+			return (1);
+	}
+	return (0);
+}
+
 char	*get_next_line(int fd)
 {
 	static char *stash;
@@ -70,8 +100,8 @@ char	*get_next_line(int fd)
 	stash = read_file(fd, stash); //read_file uses read and BUFFER_SIZE
 	if (!stash)
 		return (NULL);
-	/*if (ft_strchr(stash, '\n'))
-		return(update_data(&stash));*/
+	if (ft_strchr(stash, '\n'))
+		return(update_data(&stash));
 	return (line);
 }
 
